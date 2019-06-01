e602a9788e564db68d69cfaaadb5433830ed0489
Daniel Grenemark <daniel@grenemark.se>
2019-06-01 19:53:59 +0200

    init

---
+++ clitools/README.md
@@ -0,0 +1,14 @@
+Azure CLI
+Kubectl
+Helm
+Draft
+Docker for Desktop
+Istio
+Jq
+Pwsh
+Autocomplete??
+npm
+curl
+pandoc
+ttygif
+netcat
---
+++ hyper/install.sh
@@ -0,0 +1,9 @@
+URI=https://releases.hyper.is/download/mac
+# wget
+# extract/copy to apps
+# firacode (https://github.com/tonsky/FiraCode)
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code
+# copy settings file
+# https://ohmyz.sh/
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
